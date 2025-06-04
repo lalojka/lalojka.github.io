d7a23a4c1a3508c255b5ff6e674d2f7412de215d
lalojka <140165535+lalojka@users.noreply.github.com>
2025-06-03 22:26:42 -0300

    Update oauth.js
    modifico accesos para solicitar permiso a facebook

--- js/oauth.js
+++ js/oauth.js
@@ -5,10 +5,8 @@ const APP_ID = '493451440039423';
 const REDIRECT_URI = encodeURIComponent('https://app.epm-marketing.com/logged/');
 const SCOPE = [
   'pages_show_list',
-  'ads_management',
   'ads_read',
   'email',
-  'instagram_content_publish',
   'pages_read_engagement',
   'instagram_basic',
   'business_management',
